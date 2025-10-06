aca6a03aa049eab3cb54ab6c60ee07df641d28b7
Kelvin De Guia <deguiakelvin@gmail.com>
2025-10-06 21:56:02 +0800

    initial commit

---
+++ custom_addons/website_demo/models/web_form.py
@@ -0,0 +1,10 @@
+from odoo import models, fields
+
+class WebForm(models.Model):
+    _name = 'web.form'
+    _description = 'Web Form'
+    _rec_name ="name"
+
+    name = fields.Char(string='Form Name', required=True, store=True)
+    age = fields.Integer(string='Age', store=True)
+    description = fields.Text(string='Description', store=True)
